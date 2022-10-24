9d56cdeb335d6d99d32c516ea12798cae9d253dd
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-10-24 09:45:41 +0300

    fixed dither shader finally

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -460,133 +460,168 @@ class BlockedGlitchShader extends FlxShader
 
 class DitherShader extends FlxShader
 {
-    // couldn't find a shadertoy link srry http://devlog-martinsh.blogspot.com/2011/03/glsl-8x8-bayer-matrix-dithering.html
+    // originaly from https://github.com/hughsk/glsl-dither/blob/master/8x8.glsl
     #if SHADERS_ENABLED
     @:glFragmentSource('
         #pragma header
-        // Ordered dithering aka Bayer matrix dithering
 
-        float Scale = 1.0;
-
-        float find_closest(int x, int y, float c0)
+        float dither(vec2 position, float brightness)
         {
-            int dither[8];
-        
-            if (x == 0)
-            {
-                dither[0] = 0;
-                dither[1] = 32; 
-                dither[2] = 8;
-                dither[3] = 40;
-                dither[4] = 2;
-                dither[5] = 34;
-                dither[6] = 10;
-                dither[7] = 42;
-            }
-        
-            if (x == 1)
-            {
-                dither[0] = 48;
-                dither[1] = 16;
-                dither[2] = 56;
-                dither[3] = 24;
-                dither[4] = 50;
-                dither[5] = 18;
-                dither[6] = 58;
-                dither[7] = 26;
-            }
-        
-            if (x == 2)
-            {
-                dither[0] = 12;
-                dither[1] = 44;
-                dither[2] = 4;
-                dither[3] = 36;
-                dither[4] = 14;
-                dither[5] = 46;
-                dither[6] = 6;
-                dither[7] = 38;
-            }
-        
-            if (x == 3)
-            {
-                dither[0] = 60;
-                dither[1] = 28;
-                dither[2] = 52;
-                dither[3] = 20;
-                dither[4] = 62;
-                dither[5] = 30;
-                dither[6] = 54;
-                dither[7] = 22;
-            }
-        
-            if (x == 4)
-            {
-                dither[0] = 3;
-                dither[1] = 35;
-                dither[2] = 11;
-                dither[3] = 43;
-                dither[4] = 1;
-                dither[5] = 33;
-                dither[6] = 9;
-                dither[7] = 41;
-            }
-        
-            if (x == 5)
-            {
-                dither[0] = 51;
-                dither[1] = 19;
-                dither[2] = 59;
-                dither[3] = 27;
-                dither[4] = 49;
-                dither[5] = 17;
-                dither[6] = 57;
-                dither[7] = 25;
-            }
-        
-            if (x == 6)
-            {
-                dither[0] = 15;
-                dither[1] = 47;
-                dither[2] = 7;
-                dither[3] = 39;
-                dither[4] = 13;
-                dither[5] = 45;
-                dither[6] = 5;
-                dither[7] = 37;
-            }
-        
-            if (x == 7)
-            {
-                dither[0] = 63;
-                dither[1] = 31;
-                dither[2] = 55;
-                dither[3] = 23;
-                dither[4] = 61;
-                dither[5] = 29;
-                dither[6] = 53;
-                dither[7] = 21;
-            }
+            int x = int(mod(position.x, 8.0));
+            int y = int(mod(position.y, 8.0));
+            int index = x + y * 8;
         
             float limit = 0.0;
-
+        
             if (x < 8)
-                limit = (dither[y] + 1) / 64.0;
-
-            if (c0 < limit)
-                return 0.0;
-
-            return 1.0;
+            {
+                if (index == 0)
+                    limit = 0.015625;
+                if (index == 1)
+                    limit = 0.515625;
+                if (index == 2)
+                    limit = 0.140625;
+                if (index == 3)
+                    limit = 0.640625;
+                if (index == 4)
+                    limit = 0.046875;
+                if (index == 5)
+                    limit = 0.546875;
+                if (index == 6)
+                    limit = 0.171875;
+                if (index == 7)
+                    limit = 0.671875;
+                if (index == 8)
+                    limit = 0.765625;
+                if (index == 9)
+                    limit = 0.265625;
+                if (index == 10)
+                    limit = 0.890625;
+                if (index == 11)
+                    limit = 0.390625;
+                if (index == 12)
+                    limit = 0.796875;
+                if (index == 13)
+                    limit = 0.296875;
+                if (index == 14)
+                    limit = 0.921875;
+                if (index == 15)
+                    limit = 0.421875;
+                if (index == 16)
+                    limit = 0.203125;
+                if (index == 17)
+                    limit = 0.703125;
+                if (index == 18)
+                    limit = 0.078125;
+                if (index == 19)
+                    limit = 0.578125;
+                if (index == 20)
+                    limit = 0.234375;
+                if (index == 21)
+                    limit = 0.734375;
+                if (index == 22)
+                    limit = 0.109375;
+                if (index == 23)
+                    limit = 0.609375;
+                if (index == 24)
+                    limit = 0.953125;
+                if (index == 25)
+                    limit = 0.453125;
+                if (index == 26)
+                    limit = 0.828125;
+                if (index == 27)
+                    limit = 0.328125;
+                if (index == 28)
+                    limit = 0.984375;
+                if (index == 29)
+                    limit = 0.484375;
+                if (index == 30)
+                    limit = 0.859375;
+                if (index == 31)
+                    limit = 0.359375;
+                if (index == 32)
+                    limit = 0.0625;
+                if (index == 33)
+                    limit = 0.5625;
+                if (index == 34)
+                    limit = 0.1875;
+                if (index == 35)
+                    limit = 0.6875;
+                if (index == 36)
+                    limit = 0.03125;
+                if (index == 37)
+                    limit = 0.53125;
+                if (index == 38)
+                    limit = 0.15625;
+                if (index == 39)
+                    limit = 0.65625;
+                if (index == 40)
+                    limit = 0.8125;
+                if (index == 41)
+                    limit = 0.3125;
+                if (index == 42)
+                    limit = 0.9375;
+                if (index == 43)
+                    limit = 0.4375;
+                if (index == 44)
+                    limit = 0.78125;
+                if (index == 45)
+                    limit = 0.28125;
+                if (index == 46)
+                    limit = 0.90625;
+                if (index == 47)
+                    limit = 0.40625;
+                if (index == 48)
+                    limit = 0.25;
+                if (index == 49)
+                    limit = 0.75;
+                if (index == 50)
+                    limit = 0.125;
+                if (index == 51)
+                    limit = 0.625;
+                if (index == 52)
+                    limit = 0.21875;
+                if (index == 53)
+                    limit = 0.71875;
+                if (index == 54)
+                    limit = 0.09375;
+                if (index == 55)
+                    limit = 0.59375;
+                if (index == 56)
+                    limit = 1.0;
+                if (index == 57)
+                    limit = 0.5;
+                if (index == 58)
+                    limit = 0.875;
+                if (index == 59)
+                    limit = 0.375;
+                if (index == 60)
+                    limit = 0.96875;
+                if (index == 61)
+                    limit = 0.46875;
+                if (index == 62)
+                    limit = 0.84375;
+                if (index == 63)
+                    limit = 0.34375;
+            }
+        
+            return brightness < limit ? 0.0 : 1.0;
         }
-
-        void main(void)
+        
+        vec3 dither(vec2 position, vec3 color)
         {
-            vec4 rgba = texture2D(bitmap, openfl_TextureCoordv).rgba;
+            return color * dither(position, dot(color, vec3(0.299, 0.587, 0.114)));
+        }
         
-            int x = int(mod(vec2(gl_FragCoord.xy * Scale).x, 8.0));
-            int y = int(mod(vec2(gl_FragCoord.xy * Scale).y, 8.0));
+        vec4 dither(vec2 position, vec4 color)
+        {
+            return vec4(color.rgb * dither(position, dot(color.rgb, vec3(0.299, 0.587, 0.114))), 1.0);
+        }
         
-            gl_FragColor = vec4(find_closest(x, y, rgba.r), find_closest(x, y, rgba.g), find_closest(x, y, rgba.b), find_closest(x, y, rgba.a));
+        void main()
+        {
+            vec4 daTexture = flixel_texture2D(bitmap, openfl_TextureCoordv);
+            gl_FragColor = dither(gl_FragCoord.xy, daTexture);
         }
     ')
     #end

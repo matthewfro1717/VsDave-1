346eebf9f8068e4940e0f29fc6b2b9629bec74db
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-16 15:58:23 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -17,15 +17,15 @@ class Main extends Sprite
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var initialState:Class<FlxState> = StartStateSelector; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-
 	public static var framerate:Int = 144; // How many frames per second the game should run at.
-
 	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	public static var keyAmmo:Array<Int> = [4, 5, 6, 7, 9, 12];
+	public static var dataJump:Array<Int> = [8, 10, 12, 14, 18, 24];
 
 	public static var fps:FpsDisplay;
 
-	public static var applicationName:String = "Friday Night Funkin' | VS. Dave and Bambi 3.0b";
+	public static var applicationName:String = "Friday Night Funkin' | VS. Dave and Bambi 3.0b | Extra Keys Addon 2.0.2";
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -74,7 +74,7 @@ class Main extends Sprite
 		}
 
 		initialState = StartStateSelector;
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
+                addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
 		#if !mobile
 		fps = new FpsDisplay(10, 3, 0xFFFFFF);
